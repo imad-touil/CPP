72a2df18d17504d616f35493f9c90c51c00b68c0
imad-touil <imadtouil.it@gmail.com>
2025-10-26 20:42:02 +0100

    ss

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/26 20:31:03 by imatouil          #+#    #+#             */
+/*   Updated: 2025/10/26 20:40:34 by imatouil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+		
+}
