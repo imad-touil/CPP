d969cde32414081e92d6f49cf9e2dd55b4237c07
imad-touil <imadtouil.it@gmail.com>
2025-10-27 21:16:37 +0100

    first ex

--- traning/namespace/megaphone.cpp
+++ traning/namespace/megaphone.cpp
@@ -6,34 +6,57 @@
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 20:31:03 by imatouil          #+#    #+#             */
-/*   Updated: 2025/10/27 16:42:11 by imatouil         ###   ########.fr       */
+/*   Updated: 2025/10/27 13:10:49 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+namespace Morocco 
+{
+	int	x = 1337;
+}
+
+namespace Paris
+{
+	int	x = 42;
+}
+
+int	increment(int *x)
+{
+	*x += 1;
+	return (*x);
+}
+
+namespace Arabic
+{
+	void SayHello(std::string name)
+	{
+		std::cin >> name;
+		std::cout << "Salam Alikum, Marhba bik " << name << std::endl;
+	}
+}
+
+namespace English
+{
+	void SayHello(std::string name)
+	{
+		std::cin >> name;
+		std::cout << "Hello, Ur Welcome " << name << std::endl;
+	}
+}
+
 int	main(int ac, char **av)
 {
-	int				i;
-	int				j;
-	unsigned char	c;
+	using namespace Paris;
+	// int	x;
 
-	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		j = 0;
-		while (++j < ac)
-		{
-			i = -1;
-			while (av[j][++i])
-			{
-				c = av[j][i];
-				std::cout << (char)toupper(c);
-			}
-			std::cout << " ";
-		}
-	}
-	std::cout << std::endl;
-	return (0);
+	if (ac == 1337)
+		std::cout << av[2];
+	// x = 19;
+	// std::cout << increment(&x) << std::endl;
+	std::string name;
+	// std::cin >> name;
+	Arabic::SayHello(name);
+	English::SayHello(name);
 }
--- traning/test.cpp
+++ traning/test.cpp
@@ -1,39 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   test.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/26 20:31:03 by imatouil          #+#    #+#             */
-/*   Updated: 2025/10/27 16:42:11 by imatouil         ###   ########.fr       */
+/*   Created: 2025/10/27 17:45:38 by imatouil          #+#    #+#             */
+/*   Updated: 2025/10/27 17:48:43 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(int ac, char **av)
+int	main(void)
 {
-	int				i;
-	int				j;
-	unsigned char	c;
-
-	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		j = 0;
-		while (++j < ac)
-		{
-			i = -1;
-			while (av[j][++i])
-			{
-				c = av[j][i];
-				std::cout << (char)toupper(c);
-			}
-			std::cout << " ";
-		}
-	}
-	std::cout << std::endl;
-	return (0);
+	std::cout << "This !!" << std::endl;
+	std::cout << "equal This" << "\n" << std::flush;
+	std::cout << "But This !!" << "\n";
+	std::cout << "Doesn't Equal This" << "\n" << std::flush;
+	
 }
