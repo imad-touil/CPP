df4f37c9a25b930fdf648c2b5a2081b0f294b111
imad-touil <imadtouil.it@gmail.com>
2026-01-28 16:17:32 +0100

    CPP

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,4 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 14:36:12 by imatouil          #+#    #+#             */
+/*   Updated: 2026/01/28 16:16:11 by imatouil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
-Fixed::Fixed() : value(0) {}
+Fixed::Fixed() : value(0)
+{
+	std::cout << "Default constructor called\n";
+}
 
+Fixed::Fixed(const Fixed &obj)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = obj;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/28 14:36:30 by imatouil          #+#    #+#             */
+/*   Updated: 2026/01/28 15:05:48 by imatouil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
@@ -5,15 +17,16 @@
 
 class Fixed
 {
+	public:
+		Fixed();
+		Fixed(const Fixed &obj);
+		~Fixed();
+		int		getRawBits(void) const;
+		void	steRawBits(int const raw);
+
 	private:
 		int					value;
 		static const int	bit = 8;
-	public:
-		Fixed();
-		Fixed(const Fixed &other)
-		~Fixed();
-		int	getRawBits(void) const;
-		void	steRawBits(int const raw);
 };
 
 #endif
