c93dce7dd0eb8a020850a26c173bd05fdcb8b707
imad-touil <imadtouil.it@gmail.com>
2026-03-12 19:22:50 +0000

    CPP

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 02:46:08 by imatouil          #+#    #+#             */
+/*   Updated: 2026/03/12 14:04:46 by imatouil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		std::string	name;
+		int			grade;
+};
+
+#endif
