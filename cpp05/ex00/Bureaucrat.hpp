77ac0dc0b515817e41cad367cb6ef1394778f000
imad-touil <imadtouil.it@gmail.com>
2026-03-24 15:19:43 +0100

    CPP

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: imatouil <imatouil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 02:46:08 by imatouil          #+#    #+#             */
-/*   Updated: 2026/03/12 14:04:46 by imatouil         ###   ########.fr       */
+/*   Updated: 2026/03/12 23:24:21 by imatouil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,16 @@ class Bureaucrat
 	private:
 		std::string	name;
 		int			grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat& obj);
+		Bureaucrat&	operator=(const Bureaucrat& obj);
+		~Bureaucrat();
+
+		std::string	getName() const;
+		int			getGrade() const;
+		void		increment();
+		void		decrement();
 };
 
 #endif
